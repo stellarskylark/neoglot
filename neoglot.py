f9aee293d97519e4e2e300679a1b7ac690706dcf
Christopher Calmes <christopher.calmes@okbu.edu>
2018-10-27 15:29:25 -0500

    PEP8 compliance

--- neoglot.py
+++ neoglot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 """
 The primary executable file for Neoglot. Uses ``click`` to
@@ -15,6 +15,7 @@ from parse import Parse
 
 CATEGORIES = {}
 SYLLABLES = {}
+
 
 @click.command()
 @click.argument("file")
@@ -46,10 +47,12 @@ def run(file, count, minsylls, maxsylls):
     SYLLABLES = parser.syllables
     print_words(count, minsylls, maxsylls)
 
+
 def print_words(count, minsylls, maxsylls):
     """Prints a number of valid words."""
     for _ in range(0, count):
         click.echo(gen_word(minsylls, maxsylls))
+
 
 def gen_word(minsylls, maxsylls):
     """Generates a word with the specified number of syllables."""
@@ -57,6 +60,7 @@ def gen_word(minsylls, maxsylls):
     for _ in range(0, random.randint(minsylls, maxsylls)):
         word += gen_syll()
     return word
+
 
 def gen_syll():
     """Generates a syllable with a random syllable structure."""
@@ -74,6 +78,7 @@ def gen_syll():
         else:
             syll += cat
     return syll
+
 
 if __name__ == '__main__':
     run()
